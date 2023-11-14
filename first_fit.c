e8ee6db764e81de8fc5963eb3ad9bb94612c497c
Dip Narayan Roy <143265425+dip-0011@users.noreply.github.com>
2023-11-14 23:15:29 +0530

    Add files via upload

---
+++ first_fit.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdbool.h>
+
+// #define n_p 100
+// #define n_m 100
+
+struct mem_frag
+{
+    int size;
+    int frag;
+    bool allocated;
+};
+struct process
+{
+    int pid;
+    int size;
+    bool allocated; // process id and its memory requirement
+};
+
+int no_of_memfrag;
+int no_of_process;
+
+int main()
+{
+    
+    printf("\nEnter the number of memory fragments: ");
+    scanf("%d", &no_of_memfrag);
+    struct mem_frag memory[no_of_memfrag];
+    printf("\nEnter the number of processes: ");
+    scanf("%d", &no_of_process);
+    struct process p[no_of_process];
+
+    for (int i = 0; i < no_of_memfrag; i++)
+    {
+        printf("\nMemory fragment size of fragment %d is:\n", i + 1);
+        scanf("%d", &memory[i].size);
+        memory[i].allocated = false;
+        memory[i].frag = 0;
+    }
+
+    for (int i = 0; i < no_of_process; i++)
+    {
+        printf("\nProcess ID of Process is: ");
+        scanf("%d", &p[i].pid); // assigning values to each element in array
+        printf("\nWrite the size of the process: ");
+        scanf("%d", &p[i].size);
+        p[i].allocated = false;
+    }
+
+     for (int i = 0; i < no_of_memfrag; i++)
+    {
+        for (int j = 0; j < no_of_process; j++)
+        {
+            if ((memory[i].allocated == false) && (p[j].allocated == false))
+            {
+                if ((memory[i].size > p[j].size))
+                {
+                    memory[i].allocated = true;
+                    p[j].allocated = true;
+                    memory[i].frag = memory[i].size - p[j].size;
+                }
+                if (memory[i].size < p[j].size)
+                {
+                    printf("Fragment not available\n");
+                }
+            }
+        }
+    }
+
+    printf("\nProcess info:");
+    printf("\nProcess Id\tProcess Size\tAllocation Status");
+    for (int i = 0; i < no_of_process; i++)
+    {
+        printf("\n%d\t\t%d\t\t%d",p[i].pid,p[i].size,p[i].allocated);
+    }
+    
+    printf("\nFragment No\tFragment Size\tFragment Allocation\tFree Fragment");
+    for (int i = 0; i < no_of_memfrag; i++)
+    {
+        printf("\n%d\t\t%d\t\t%d\t\t%d",i+1,memory[i].size,memory[i].allocated,memory[i].frag);
+    }
+    
+
+    return 0;
+}
